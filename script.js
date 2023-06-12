693472a48b513af162299607c0e3074531099fca
Gabriel Fernandes Baltazar <gabriel.baltazar555@rede.ulbra.br>
2023-06-12 20:23:37 -0300

    esse é um projeto ultilizao para meu encino pessoal

---
+++ script.js
@@ -0,0 +1,33 @@
+function ola(){
+alert('ola.')
+}
+
+function olaPessoa(nome){
+    alert( nome + ' seja bem vindo')
+}
+
+function saudacao(){
+    const nome= document.getElementById('nome').velue
+    alert('saudações nobre engenheiro ' + nome)
+    
+}
+
+function mauseSobre(){
+    alert('o mouse esta em cima')
+}
+function mouseFora(){
+    alert('maue fugio')
+}
+
+function textoModificado(){
+    alert(' o texto foi lterado')
+}
+function emFoco(){
+    const user = document.getElementById("user")
+    user.style.border= '3px solid green'
+    user.style.background= 'grey'
+}
+function semFoco(){
+    user.style.border = '1px solid blck'
+    user.style.background ='white'
+}
